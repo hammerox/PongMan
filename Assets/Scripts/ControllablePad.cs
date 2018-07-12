3c9a9ca6aaba534c28fc51e8a150d13a696ab8fa
mcustodio <mauricioramalho2@gmail.com>
2018-07-11 21:53:43 -0300

    Pads now hit the wall

--- Assets/Scripts/ControllablePad.cs
+++ Assets/Scripts/ControllablePad.cs
@@ -6,10 +6,18 @@ public class ControllablePad : MonoBehaviour {
 
     public float speedMultiplier;
 
+    private float minPosition;
+    private float maxPosition;
+    private Collider2D collider;
+
     // Use this for initialization
     void Start () {
-		
-	}
+        collider = GetComponent<Collider2D>();
+        float vertExtent = Camera.main.orthographicSize;
+        float horzExtent = vertExtent * Screen.width / Screen.height;
+        minPosition = -horzExtent + collider.bounds.extents.x;
+        maxPosition = horzExtent - collider.bounds.extents.x;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -19,6 +27,25 @@ public class ControllablePad : MonoBehaviour {
     private void FixedUpdate()
     {
         float xInput = Input.GetAxis("Horizontal") * speedMultiplier;
-        transform.Translate(new Vector2(xInput * speedMultiplier, 0.0f));
+        float nextPosition = xInput + transform.position.x;
+        print(nextPosition);
+        bool isLeftOfScreen = nextPosition < minPosition;
+        bool isRightOfScreen = nextPosition > maxPosition;
+
+        if (isLeftOfScreen)
+        {
+            // Hit left wall
+            transform.position = new Vector2(minPosition, transform.position.y);
+
+        } else if (isRightOfScreen)
+        {
+            // Hit right wall
+            transform.position = new Vector2(maxPosition, transform.position.y);
+
+        } else
+        {
+            // Move normally
+            transform.Translate(new Vector2(xInput, 0.0f));
+        }
     }
 }
